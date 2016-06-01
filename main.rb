4f2a6709856e9c3f726069de382ede5afd9ada55
jsvelasquezv <juseve200@gmail.com>
2016-05-31 20:30:40 -0500

    Generacion de nuevos problemas con restricciones adicionales lista

--- main.rb
+++ main.rb
@@ -25,39 +25,46 @@ print modelo.RHS
 problemas = []
 no_entero = []
 
-puts solucion
+# p solucion
 
-# solucion.each_with_index { |valor, indice| break if valor.modulo(1) != 0; no_entero[indice] = valor }
-
-# puts solucion.select { |item| item.modulo(1) != 0}
-
-
-
-# puts no_entero[0]
-# puts no_entero[1]
-# puts no_entero[2]
-
-# restriccion1 = {"x#{no_entero[0]}" => -no_entero[1].ceil}
-# restriccion2 = {"x#{no_entero[0]}" => no_entero[1].floor}
-
-=begin
-problema1 = modelo.clone()
-problema2 = modelo.clone()
-
-problema1.agregar_restriccion(-1, "x#{no_entero[0]}", -no_entero[1].ceil)
-problema2.agregar_restriccion(1, "x#{no_entero[0]}", no_entero[1].floor)
-
-problemas.push(problema1)
-problemas.push(problema2)
-
-problemas[0].calcular_solucion
-problemas[1].calcular_solucion
-
-puts problemas[0].solucion
-puts problemas[1].solucion
-=end
+no_entero[0] = solucion.each_with_index { |valor, indice| break valor if valor.modulo(1) != 0}
+no_entero[1] = solucion.find_index(no_entero[0])
+# p no_entero
 
 
+problema1 = Marshal.load(Marshal.dump(modelo))
+problema2 = Marshal.load(Marshal.dump(modelo))
+# problema1.inicializar
+# problema2 = Modelo.new
+# problema2.inicializar
+
+# p problema1.restricciones
+# puts "\n"
+
+problema1.agregar_restriccion(-1, "x#{no_entero[1]}", -no_entero[0].ceil)
+problema2.agregar_restriccion(1, "x#{no_entero[1]}", no_entero[0].floor)
+
+# p problema1.restricciones
+
+# p problema1.restricciones
+# p problema1.RHS
+# puts "\n"
+problema1.calcular_solucion
+problema2.calcular_solucion
+
+p problema1.solucion
+puts problema1.z
+p problema2.solucion
+puts problema2.z
+
+# problemas.push(problema1)
+# problemas.push(problema2)
+
+# problemas[0].calcular_solucion
+# problemas[1].calcular_solucion
+
+# p problemas[0].solucion
+# p problemas[1].solucion
 
 # puts restriccion1
 # puts restriccion2
